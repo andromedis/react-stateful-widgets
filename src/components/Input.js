3b20d2a8b08abd938b1f0e2ffd6c1df2705e8849
Rebecca Peckman <rebeccapeckman@gmail.com>
2021-02-15 20:55:09 +0000

    Squares.js done

--- src/components/Input.js
+++ src/components/Input.js
@@ -44,6 +44,7 @@ export default function Input() {
     // When the input changes, its whole value can be found inside the event object.
     // Log out the synthetic event object 'evt' and see for yourself.
     const { value } = evt.target;
+    console.log(evt);
 
     /* STEP 4 */
     setInputValue(value);
